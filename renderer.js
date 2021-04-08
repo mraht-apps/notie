f92869d00ba22fc6fecf361f26b3cc14862a3e64
mariusraht2 <mariusraht+2@gmail.com>
2021-04-08 17:46:48 +0200

    Popup to add edit blocks (on-going)

--- renderer.js
+++ renderer.js
@@ -100,3 +100,37 @@ $("#btnRestart").on("click", function (event) {
 
 let textline = new Textline($("#pageContent"));
 textline.build();
+
+
+
+
+
+
+
+
+function deselect(e) {
+  $('.pop').slideFadeToggle(function () {
+    e.removeClass('selected');
+  });
+}
+
+$(function () {
+  $('#contact').on('click', function () {
+    if ($(this).hasClass('selected')) {
+      deselect($(this));
+    } else {
+      $(this).addClass('selected');
+      $('.pop').slideFadeToggle();
+    }
+    return false;
+  });
+
+  $('.close').on('click', function () {
+    deselect($('#contact'));
+    return false;
+  });
+});
+
+$.fn.slideFadeToggle = function (easing, callback) {
+  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+};
